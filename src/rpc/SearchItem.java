25f1ae395f649846919ef6f4e8603bce6195ee09
twanging <wang573@usc.edu>
2017-10-26 20:23:01 -0700

    First commit.

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,148 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+import external.ExternalAPI;
+import external.ExternalAPIFactory;
+import external.TicketMasterAPI;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		// TODO Auto-generated method stub
+		//Create a PrintWriter from response such that we can add data to response.
+				/**PrintWriter out = response.getWriter();
+				if (request.getParameter("username") != null) {
+					//Get the username sent from the client
+					String username = request.getParameter("username");
+		            //In the output stream, add something to response body. 
+					out.print("Hello " + username);
+				}
+				// Send response back to client
+				out.flush();
+				out.close();*/
+		/**response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>This is a HTML page</h1>");
+		out.println("</body></html>");
+		out.flush();
+		out.close();*/
+		/**response.setContentType("application/json");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+
+		String username = "";
+		if (request.getParameter("username") != null) {
+			username = request.getParameter("username");
+		}
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("username", username);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.flush();
+		out.close();*/
+		
+		//response.setContentType("application/json");
+		//response.addHeader("Access-Control-Allow-Origin", "*");
+		//PrintWriter out = response.getWriter();
+
+		/**JSONArray array = new JSONArray();
+		try {
+			array.put(new JSONObject().put("username", "abcd"));
+			array.put(new JSONObject().put("username", "1234"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		//out.print(array);
+		//out.flush();
+		//out.close();
+      Rpchelper.writeJsonArray(response, array);*/
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Get parameter from HTTP request
+		//String userId = request.getParameter("user_id");double lat = Double.parseDouble(request.getParameter("lat"));
+	  //  double lon = Double.parseDouble(request.getParameter("lon"));
+	  //  String term = request.getParameter("term"); // term can be null
+
+       // call TicketMasterAPI.search to get event data
+	   // TicketMasterAPI tmAPI = new TicketMasterAPI();
+	    //List<Item> items = tmAPI.search(lat, lon, term);
+	    // Connect to external API
+	    //ExternalAPI api = ExternalAPIFactory.getExternalAPI();
+	   // List<Item> items = api.search(lat, lon, term);
+
+	    // There should be some saveItem logic here
+	    
+	    // Convert Item list back to JSONArray for client
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term"); // Term can be empty or null.
+		
+		DBConnection conn = DBConnectionFactory.getDBConnection();
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
+		List<JSONObject> list = new ArrayList<>();
+
+		Set<String> favorite = conn.getFavoriteItemIds(userId);
+		try {
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				if (favorite != null) {
+					obj.put("favorite", favorite.contains(item.getItemId()));
+				}
+				list.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    JSONArray array = new JSONArray(list);
+	    Rpchelper.writeJsonArray(response, array);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
